465acce491c749743382d804af9e93fb15324e00
xugan <xugan@192.168.0.103>
2024-09-02 20:59:50 +0800

    init

---
+++ src/main/java/com/word/WordClearApplication.java
@@ -0,0 +1,13 @@
+package com.word;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordClearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordClearApplication.class, args);
+    }
+
+}
