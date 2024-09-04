3086049a9f1848217676c55081bc793c34cefa75
xugan <xugan@cctv-5.lan>
2024-09-04 15:22:33 +0800

    clear

--- src/main/java/com/word/controller/FileUploadController.java
+++ src/main/java/com/word/controller/FileUploadController.java
@@ -1,37 +1,23 @@
 package com.word.controller;
 
-import cn.hutool.core.util.StrUtil;
-import com.alibaba.fastjson2.JSON;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.hwpf.HWPFDocument;
-import org.apache.poi.hwpf.model.SEPX;
-import org.apache.poi.hwpf.model.SectionTable;
 import org.apache.poi.hwpf.usermodel.HeaderStories;
 import org.apache.poi.hwpf.usermodel.Paragraph;
 import org.apache.poi.hwpf.usermodel.Range;
-import org.apache.poi.hwpf.usermodel.Section;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.xwpf.usermodel.*;
-import org.openxmlformats.schemas.drawingml.x2006.picture.CTPicture;
-import org.openxmlformats.schemas.drawingml.x2006.wordprocessingDrawing.CTInline;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTDrawing;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTR;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFFooter;
+import org.apache.poi.xwpf.usermodel.XWPFHeader;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -85,33 +71,6 @@ public class FileUploadController {
         }
     }
 
-    private void processDocFile(ByteArrayInputStream bais, ZipOutputStream zipOut, String fileName) throws IOException {
-        try (HWPFDocument document = new HWPFDocument(bais);) {
-            // 获取所有页眉
-//            HeaderStories headerStories = new HeaderStories(document);
-
-//            // 清除所有页眉内容
-//            clearHeader(headerStories.getFirstHeaderSubrange()); // 清除首页页眉
-//            clearHeader(headerStories.getOddHeaderSubrange()); // 清除奇数页页眉
-//            clearHeader(headerStories.getEvenHeaderSubrange()); // 清除偶数页页眉
-//
-//            // 清除所有页眉内容
-//            clearHeader(headerStories.getFirstFooterSubrange()); // 清除首页页眉
-//            clearHeader(headerStories.getOddFooterSubrange()); // 清除奇数页页眉
-//            clearHeader(headerStories.getEvenFooterSubrange()); // 清除偶数页页眉
-
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            document.write(baos);
-
-            ZipEntry zipEntry = new ZipEntry(fileName);
-            zipOut.putNextEntry(zipEntry);
-            zipOut.write(baos.toByteArray());
-            zipOut.closeEntry();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     // 处理 .doc 文件
     private void processDocFile(InputStream inputStream, ZipOutputStream zipOut, String fileName) throws IOException {
         try (HWPFDocument document = new HWPFDocument(inputStream)) {
@@ -127,26 +86,19 @@ public class FileUploadController {
             zipOut.closeEntry();
         }
     }
+
     // 去除 .doc 文件的页眉和页脚
     private void removeHeadersAndFooters(HWPFDocument document) {
         HeaderStories headerStories = new HeaderStories(document);
 
-        // 清除首页页眉
+        // 清除 首页/奇数页/偶数页 页眉
         clearHeaderOrFooter(headerStories.getFirstHeaderSubrange());
-
-        // 清除奇数页页眉
         clearHeaderOrFooter(headerStories.getOddHeaderSubrange());
-
-        // 清除偶数页页眉
         clearHeaderOrFooter(headerStories.getEvenHeaderSubrange());
 
-        // 清除首页页脚
+        // 清除 首页/奇数页/偶数页 页脚
         clearHeaderOrFooter(headerStories.getFirstFooterSubrange());
-
-        // 清除奇数页页脚
         clearHeaderOrFooter(headerStories.getOddFooterSubrange());
-
-        // 清除偶数页页脚
         clearHeaderOrFooter(headerStories.getEvenFooterSubrange());
     }
 
@@ -163,7 +115,5 @@ public class FileUploadController {
             }
         }
     }
-
-
 
 }
